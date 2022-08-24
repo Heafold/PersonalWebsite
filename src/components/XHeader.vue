54cce24b863669220ec5903b916f37809fe5f153
Heafold <mcorentin62@gmail.com>
2022-08-24 17:28:44 +0200

    adding-tailwind

---
+++ src/components/XHeader.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <div class="text-center">
+        <h1 class="text-3xl">TEST</h1> 
+    </div>
+    
+</template>
+
+<script>
+export default {
+  name: 'XHeader',
+  props: {
+   
+  }
+}
+</script>
