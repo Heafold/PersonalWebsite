4ec6aa50d753f13f491d68647ad9c47271fc2432
Heafold <mcorentin62@gmail.com>
2022-08-26 13:26:20 +0200

    HomePage

--- src/components/XHeader.vue
+++ src/components/XHeader.vue
@@ -3,10 +3,10 @@
     <div class="flex">
         <h1 class="text-2xl text-white cursor-pointer">> CM.</h1>
         <div class="flex ml-32 justify-around w-3/12">
-            <h2 class="text-slate-400 hover:text-slate-200 cursor-pointer hover:bg-slate-800 w-20 text-center p-1 ">Accueil</h2>
-            <h2 class="text-slate-400 hover:text-slate-200 cursor-pointer hover:bg-slate-800 w-20 text-center p-1 ">A propos</h2>
-            <h2 class="text-slate-400 hover:text-slate-200 cursor-pointer hover:bg-slate-800 w-20 text-center p-1 ">Projets</h2>
-            <h2 class="text-slate-400 hover:text-slate-200 cursor-pointer hover:bg-slate-800 w-20 text-center p-1 ">Contact</h2>
+            <h2 class="text-slate-400 hover:text-slate-200 cursor-pointer hover:bg-slate-800 w-20 text-center p-1 rounded ">Accueil</h2>
+            <h2 class="text-slate-400 hover:text-slate-200 cursor-pointer hover:bg-slate-800 w-20 text-center p-1 rounded">A propos</h2>
+            <h2 class="text-slate-400 hover:text-slate-200 cursor-pointer hover:bg-slate-800 w-20 text-center p-1 rounded ">Projets</h2>
+            <h2 class="text-slate-400 hover:text-slate-200 cursor-pointer hover:bg-slate-800 w-20 text-center p-1 rounded">Contact</h2>
         </div>
     </div>
     
--- src/components/XHome.vue
+++ src/components/XHome.vue
@@ -1,10 +1,22 @@
 <template>
-
-    <div class="flex mt-32 ml-64">
-        <div class="memoji w-40 h-40 bg-slate-700  shadow rounded-full">
-            
+    <div class="flex">
+      <div class="flex mt-[5%] ml-[20%]">
+            <div class="memoji w-40 h-40 bg-slate-700  shadow rounded-full"></div>
         </div>
+    <div class="text-white mt-[8%] ml-[5%]">
+        <h3 class="text-5xl">Bonjour 👋,</h3>
+        <h3 class="text-5xl">Je suis Corentin Maese.</h3>
+        <p class="mt-5 text-2xl">Développeur Front-End Vue.Js Junior</p>
+        <p class="text-2xl">passionné par le développement Web/Web mobile.</p>
+        <div class="flex">
+            <div class="w-24 bg-slate-600 hover:bg-slate-700 p-1 text-center rounded mt-5 cursor-pointer duration-150 ">Explorer ></div>
+            <div class="w-28 bg-red-600 hover:bg-red-700 p-1 text-center rounded mt-5 ml-5 cursor-pointer duration-150">Me contacter</div>
+            <a href="https://github.com/Heafold" target="_blank" class="github w-5 h-5 mt-[4.5%] ml-5"></a>
+        </div>
+        
+    </div>  
     </div>
+    
     
 </template>
 
@@ -23,5 +35,9 @@ export default {
         background-image: url('../assets/memoji.png');
         background-size: cover;
         background-position-y: -25px;
+    }
+    .github{
+        background-image: url('../assets/github.png');
+        background-size: cover;
     }
 </style>
